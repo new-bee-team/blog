907cba1715a01b19662c58611baca6c02753b3af
PigPigBoy <1359862182@qq.com>
2020-05-25 14:13:10 +0800

    项目初始化

---
+++ user-provider-7000/src/main/java/group1/userprovider7000/UserProvider7000Application.java
@@ -0,0 +1,13 @@
+package group1.userprovider7000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProvider7000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProvider7000Application.class, args);
+    }
+
+}
---
+++ user-provider-7000/src/test/java/group1/userprovider7000/UserProvider7000ApplicationTests.java
@@ -0,0 +1,13 @@
+package group1.userprovider7000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProvider7000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
