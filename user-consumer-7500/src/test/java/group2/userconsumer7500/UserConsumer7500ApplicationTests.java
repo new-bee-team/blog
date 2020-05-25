7d1026beb6dbf2e607cfa14f35ae075561bce7cd
PigPigBoy <1359862182@qq.com>
2020-05-25 18:23:27 +0800

    项目初始化

---
+++ user-consumer-7500/src/test/java/group2/userconsumer7500/UserConsumer7500ApplicationTests.java
@@ -0,0 +1,13 @@
+package group2.userconsumer7500;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserConsumer7500ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
